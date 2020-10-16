9a2ef75bf3ba74e3bbbe740687fac987bd35c46e
Oleg Andreyev <oleg@andreyev.lv>
2020-10-16 13:27:38 +0300

    fixes #5
    added negative check before doing slice

--- src/AccessHelper.php
+++ src/AccessHelper.php
@@ -90,7 +90,7 @@ class AccessHelper
         }
 
         if (is_array($collection)) {
-            if (is_int($key)) {
+            if (is_int($key) && $key < 0) {
                 return array_slice($collection, $key, 1, false)[0];
             }
 
