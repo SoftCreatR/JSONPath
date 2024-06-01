272173a65fd16b25010dbd54d04dd34c0c5a8500
Sascha Greuel <github@1-2.dev>
2024-06-01 11:15:21 +0200

    0.9.1

--- src/Filters/QueryMatchFilter.php
+++ src/Filters/QueryMatchFilter.php
@@ -10,6 +10,7 @@ namespace Flow\JSONPath\Filters;
 
 use Flow\JSONPath\AccessHelper;
 use Flow\JSONPath\JSONPath;
+use Flow\JSONPath\JSONPathException;
 use RuntimeException;
 
 class QueryMatchFilter extends AbstractFilter
@@ -19,6 +20,9 @@ class QueryMatchFilter extends AbstractFilter
       (\s*(?<operator>==|=~|=|<>|!==|!=|>=|<=|>|<|in|!in|nin)\s*(?<comparisonValue>.+))?
     ';
 
+    /**
+     * @throws JSONPathException
+     */
     public function filter($collection): array
     {
         \preg_match('/^' . static::MATCH_QUERY_OPERATORS . '$/x', $this->token->value, $matches);
@@ -61,13 +65,15 @@ class QueryMatchFilter extends AbstractFilter
 
         foreach ($collection as $value) {
             $value1 = null;
+
             if (AccessHelper::keyExists($value, $key, $this->magicIsAllowed)) {
                 $value1 = AccessHelper::getValue($value, $key, $this->magicIsAllowed);
             } elseif (\str_contains($key, '.')) {
                 $value1 = (new JSONPath($value))->find($key)->getData()[0] ?? '';
             }
+
             if ($value1) {
-                if ($operator === null && $value1) {
+                if ($operator === null) {
                     $return[] = $value;
                 }
 
